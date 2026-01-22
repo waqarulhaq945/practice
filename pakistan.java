da0af7e93fb623d685908c8fa01ccee3c91dbb5d
waqarulhaq945 <waqarulhaq945@gmail.com>
2026-01-22 14:14:21 +0500

    Add files via upload

---
+++ pakistan.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+class pakistan
+{
+    public static void main(String[]args)
+    {
+        /*  Scanner sos = new Scanner(System.in);
+         String A, B, C;
+         System.out.println("enter no:");
+          A = sos.nextLine();
+
+         System.out.println("enter no again:");
+         B = sos.nextLine();
+
+         C = A + B;
+         System.out.println("the sum of both no is:" + C);
+        */
+       /* 
+         String pakistan = "   Pakistan Zindabad    ";
+         System.out.println(pakistan.length());
+         System.out.println(pakistan.toUpperCase());
+            System.out.println(pakistan.toLowerCase());
+            System.out.println("the character at 8:" + pakistan.charAt(7));
+            System.out.println(pakistan.replace("Zindabad", "paindabad"));
+            System.out.println(pakistan.indexOf("Zindabad"));   
+            System.out.println("before: [" + pakistan + "]");
+            System.out.println("after: [" + pakistan.trim() + "]");
+            */
+           /* 
+            String words = "hello";
+            String words2 = "hello";
+            String words3 = "HELLO";
+            String words4 = "HELLo";
+            System.out.println(words.equals(words2));
+            System.out.println(words.equals(words3));
+            System.out.println(words.equals(words4));
+            System.out.println(words.equalsIgnoreCase(words3));
+            System.out.println(words.equalsIgnoreCase(words4));
+            */
+           /* 
+           String string1 = "Pakistan";
+           String string2 = "India";
+           String string3 = "China";
+           String string4 = "Afghanistan";
+          int result1 = string1.compareTo(string2);
+           System.out.println(result1);
+           int result2 = string1.compareTo(string3);
+           System.out.println(result2);
+           int result3 = string3.compareTo(string4);      
+           System.out.println(result3);
+           int result4 = string1.compareTo(string4);    
+              System.out.println(result4);      
+              int result5 = string1.compareToIgnoreCase(string2);
+           System.out.println(result5);  
+           */
+         /*  String str1 = "Car";
+            int i1 = str1.codepointAt(1);
+            System.out.println("The Unicode value of " + str1 + " is: " + i1);
+            sir se pouchna hn nhi horaha
+            */
+           /*
+           String str = "pakistan";
+           String str2 = "zindabad";
+           System.out.println(str.concat(" " + str2));
+            */
+           /* 
+            String mainString = "Pakistan Zindabad";
+            String sub = "Pakistan";
+            boolean result = mainString.contains(sub);
+            System.out.println("Does the main string contain the substring? " + result);
+            */
+           /* 
+           String P1 = "hello world";
+           String sub1 = P1.substring(6);
+           System.out.println("The substring is: " + sub1);
+           String sub2 = P1.substring(0,5);
+           System.out.println("The substring is: " + sub2);
+           */
+          /* 
+          String P1 = "hello world";
+          boolean sub1 = P1.startsWith("hello");
+          System.out.println("The substring is: " + sub1);
+          boolean sub2 = P1.endsWith("world");
+          System.out.println("The substring is: " + sub2);
+            */
+           /* 
+              String P1 = "hello world hello universe";
+              String words[] = P1.split(",");
+                System.out.println("The words are: ");  
+                for (String w : words) 
+                {
+                    System.out.println("-" + w);
+                } 
+                    sir se pouchna hn nhi horaha
+            */
+           /* 
+            String P1 = " \n hello world hello universe \t ";
+            String strippedString = P1.strip();
+            System.out.println("Original String: [" + P1 + "]");
+            System.out.println("Stripped String: [" + strippedString + "]");              
+            */
+           String pakistan = "454484545454pakistan454545454";
+        System.out.println(pakistan.lastIndexOf("pakistan"));
+
+        
+    }
+}
